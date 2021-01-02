354cd25a26c955751fcbddb8b1c169858ac5231d
VictoriaMarjina <61832815+VictoriaMarjina@users.noreply.github.com>
2021-01-02 20:56:54 +0400

    mess

--- src/modules/messenger/Messenger.jsx
+++ src/modules/messenger/Messenger.jsx
@@ -12,14 +12,24 @@ export default class Messenger extends React.PureComponent {
 
     render() {
         return (
-            <div className={'wrapper'}>
-                <div className={'wrapper__header'}>
-                    <span children={'Header'} />
-                </div>
-                <div className={'wrapper__body'}>
-
+            <div className = {"main-conatiner"}>           
+            <div className = {'main-container__header'}>
+                <button className={"main-container__log-out"} children={"X"}>
+                </button>
+                <div className={"header__window-message"}>
+                    <div>
+                        <ul className={"ul__li"}>
+                            <li className={"message-li__message-ul"}></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
+            <div className={"main-container__footer"}>
+                <input className={" footer__input-footer"}></input>
+                <button className={" footer__button-footer"} children={"Send"}>
+                </button>
+            </div>
+        </div>
         )
     }
 }
